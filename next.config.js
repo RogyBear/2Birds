67004662b13757e6c34aaff50f7cbf25af98b862
Aaron <rogersac24@gmail.com>
2020-01-16 19:07:23 +0500

    adding updated next config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
 const webpack = require('webpack');
+require('dotenv').config();
 module.exports = withCSS(
 	withSass({
 		webpack(config, options) {
@@ -14,23 +15,19 @@ module.exports = withCSS(
 				}
 			});
 
-			// config.node = {
-			// 	fs: 'empty'
-			// };
+			config.node = {
+				fs: 'empty'
+			};
+			
+			const env = Object.keys(process.env).reduce((acc, curr) => {
+				acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+				return acc;
+			}, {});
 
-			// const env = Object.keys(process.env).reduce((acc, curr) => {
-			// 	acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
-			// 	return acc;
-			// }, {});
-
-			// config.plugins.push(new webpack.DefinePlugin(env));
+			
+			config.plugins.push(new webpack.DefinePlugin(env));
 			return config;
-		},
-		env: {
-			CMSLINK: ' http://localhost:1337',
-			CMSLINKORDERS: 'http://localhost:1337/orders',
-			CMSLINKEMAILS: 'http://localhost:1337/email',
-			CMSLINKSTORES: 'http://localhost:1337/stores'
 		}
+		
 	})
 );
