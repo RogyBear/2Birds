404162655fd7c22ff4b0823116e14aafd0bc7552
Aaron <rogersac24@gmail.com>
2020-01-16 18:22:52 +0500

    adding updated about.js

--- components/About.js
+++ components/About.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext } from 'react';
+import React, { useContext } from 'react';
 import { LanguageContext } from '../contexts/LanguageContext';
 
 const About = (props) => {
